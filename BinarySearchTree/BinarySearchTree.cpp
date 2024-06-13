89195cbbeb99323ba12ee46f139ce794322afd4d
tacosz <nabilnasruddin.offc@gmail.com>
2024-06-13 18:29:52 +0700

    memasukkan nilai ke data field sebagai data baru

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -31,6 +31,7 @@ public:
     void insert(string element) //insert a node in the binary search tree
     {
         Node* newNode = new Node(element, nullptr, nullptr); //allocate memory for the new node
+        newNode->info = element; //assign value to the data field of the new data
     }
 };
 

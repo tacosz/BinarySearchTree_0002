28db39e05f46ab13976188840bb4b26269684a6e
tacosz <nabilnasruddin.offc@gmail.com>
2024-06-13 18:46:07 +0700

    fungsi else if guna data field dari new data lebih dari parent membuat anak kanan darri point orang tua ke node baru

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -45,9 +45,13 @@ public:
             return; //exit
         }
 
-        if (element < parent->info) //If the value in the data field pf the new node is less than that of the parent
+        if (element < parent->info) //If the value in the data field of the new node is less than that of the parent
         {
             parent->leftchild = newNode; // Make the left child of the parent point to the new node
+        }
+        else if (element > parent->info) // If the value in the data field of the new data is greater than that of the parent
+        {
+            parent->rightchild = newNode; //Make the right child of the parent point to the new node
         }
     }
 };

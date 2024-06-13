9b078de002cd809a649fc0e634b96834d5939e08
tacosz <nabilnasruddin.offc@gmail.com>
2024-06-13 18:31:58 +0700

    right node dari newnode ke point null

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -33,6 +33,7 @@ public:
         Node* newNode = new Node(element, nullptr, nullptr); //allocate memory for the new node
         newNode->info = element; //assign value to the data field of the new data
         newNode->leftchild = nullptr; //make the left child of the new node point to NULL
+        newNode->rightchild = nullptr; //make the right child of the new node point to NULL
     }
 };
 
